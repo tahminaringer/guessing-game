651059c2f057121a415103d71c2200a830d51ca4
Tahmina Ringer <tahmina.ringer@gmail.com>
2020-09-03 19:54:25 -0700

    made adjustments to lab 04

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-'use strict'
+'use strict';
 
 var totalCorrectAnswers = 0;
 
 //console.log('js is loaded');
 
 var name = prompt('What is your first name?');
-       
+
 alert('Nice to meet you ' + name + '!');
 //console.log(name);
 //console.log("Hello" + name);
 
 
 //console.log(ready);
-var question1 = "Do you like puppies?";
-var question2 = "Is pizza your favorite food?";
-var question3 = "Is Magarita your favorite pizza?";
-var question4 = "Am I from Montana?";
-var question5 = "Do I love flowers?";
+var question1 = 'Do you like puppies?';
+var question2 = 'Is pizza your favorite food?';
+var question3 = 'Is Magarita your favorite pizza?';
+var question4 = 'Am I from Montana?';
+var question5 = 'Do I love flowers?';
 
 // remember that question == var question1-5
 
@@ -24,6 +24,7 @@ function aboutMe(question,alertYes,alertNo) {
   question = prompt(question).toLowerCase();
   if(question === 'yes') {
     alert(alertYes);
+    totalCorrectAnswers++;
     // console.log(alertYes);
   } else {
     alert(alertNo);
@@ -31,23 +32,34 @@ function aboutMe(question,alertYes,alertNo) {
   }
 }
 
-aboutMe(question1,"Puppies are the cutest!","What?!");
-aboutMe(question2,"Me too!","We do not agree");
-aboutMe(question3, "I like Magarita too!","That's silly!");
-aboutMe(question4,"I am from Montana!","Actually, I grew up in Montana");
-aboutMe(question5, "Yes, dahlias are my favorite!","What's wrong with you?!");
+aboutMe(question1,'Puppies are the cutest!','What?!');
+aboutMe(question2,'Me too!','We do not agree');
+aboutMe(question3, 'I like Magarita too!','That\'s silly!');
+aboutMe(question4,'I am from Montana!','Actually, I grew up in Montana');
+aboutMe(question5, 'Yes, dahlias are my favorite!','What\'s wrong with you?!');
 
-
-
-    
 alert('Thank you for playing ' + name + 'I hope you enjoyed getting to know me better');
 
-// question 6  
-function numberGame(number,max) {
+// question 6
+function askUser () {
+  var question6 = prompt('Want to play another game?');
+  if (question6 === 'yes') {
+    var answer = ('5');
+    numberGame(answer,4);
+  } else if(question6 === 'no') {
+    alert('Good bye ' + name);
+    return;
+  }
+}
+askUser();
+
+
+
+function numberGame(number, max) {
   for (var i = 0; i < max; i = i + 1){
     var userGuess = prompt('Guess a number between 1-50');
     if(userGuess === number){
-      alert('great job ' + name + "! " + 'You are correct!');
+      alert('great job ' + name + '! ' + 'You are correct!');
       totalCorrectAnswers++;
       break;
     } else if(userGuess < number){
@@ -55,37 +67,47 @@ function numberGame(number,max) {
     } else if (userGuess > number){
       alert('Bummer ' + name + ', ' + 'you guessed too high!');
     }
-    if(i === max){
+    if(i === max -1){
       alert(name + 'the correct answer is ' + number);
+      break;
+    }
+  }
+  var answer7 = ['flowers', 'nature', 'family', 'friends', 'gardening', 'hiking'];
+  aboutMeGame(answer7,6);
+}
+
+// var question6 = prompt('Want to play another game?');
+// question 7
+
+
+
+function aboutMeGame(answer7,max) {
+  // console.log(answer7);
+  // console.log(max);
+  for (var i = 0; i < max; i++) {
+    var userGuess7 = prompt('Name one of my favorite things?');
+    // console.log(answer7[i]);
+    //this for loop checks potential answers
+    var answeredCorrect = false;
+    for (var j = 0; j < answer7.length; j++) {
+      if (userGuess7 === answer7[j]) {
+        alert('You guessed right!' + 'Here are all the correct answers! ' + answer7);
+        totalCorrectAnswers++;
+        answeredCorrect = true;
+      }
+    }
+    if (answeredCorrect === false) {
+      if (i === 5) {
+        alert('You guessed wrong, but here are the right answers! ' + answer7);
+      } else {
+        alert('Guess again');
+      }
+
+    } else if (answeredCorrect === true) {
+      break;
     }
   }
 }
-    var question6 = prompt('Want to play another game?');
-    var answer = ('5');
-    numberGame(answer,4);
-
-
-// question 7
-var answer7 = ['flowers', 'nature', 'family', 'friends', 'gardening', 'hiking'];
-
-function aboutMeGame(answer,max) {
-    for (var i = 0; i < max; i++) {
-        var userGuess7 = prompt('Name one of my favorite things?');
-        console.log(answer[i]);
-        if (answer.includes(userGuess7.toLowerCase())) {
-            alert('You guessed right!');
-            totalCorrectAnswers++;
-            break;
-        } else if(i === 5 && !answer.includes(userGuess7.toLowerCase())) {
-            alert('You guessed wrong, but here are the right answers! ' + answer);
-        } else {
-            alert('Guess again');
-        }
-    }
-}
-
-aboutMeGame(answer7,6);
-
 
 
 
